4066d8d181d3e9fbd99d701dcdf202a05d7b1a80
bjoernh2000 <bjoernh@bu.edu>
2021-09-08 21:25:58 -0400

    fixed launch.sh

--- launch.sh
+++ launch.sh
@@ -2,6 +2,6 @@
 
 export FLASK_APP=project.server
 export APP_SETTINGS="project.server.config.ProductionConfig"
-# flask db init
-# flask db migrate
+flask db init
+flask db migrate
 flask db upgrade
